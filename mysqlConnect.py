2e8095c74b8e4e38f3b7b813871b0b1f0d52f48b
Boughton <nboughton+github@pm.me>
2020-10-06 16:45:49 -0400

    fixed player update

--- mysqlConnect.py
+++ mysqlConnect.py
@@ -1,11 +1,12 @@
 import mysql.connector, json, time
 
 db = mysql.connector.connect(
-    host="1.1.1.1",
+    host="1.1.1.1,
     user="perun",
-    password="perun",
-    database="perun"
+    password="test",
+    database="test"
     )
+db.autocommit = True
 
 def getPlayerlist():
     mycursor = db.cursor()
@@ -19,7 +20,6 @@ def getPlayerCount(instance):
     myresult = mycursor.fetchone()
     playerCount = json.loads(myresult[2])["c_players"]
     playerCount = playerCount - 1
-    db.commit()
     return playerCount
 
 def getCurrentMission(instance):
@@ -27,13 +27,11 @@ def getCurrentMission(instance):
     mycursor.execute("SELECT * from pe_dataraw WHERE pe_dataraw_type = 2 AND pe_dataraw_instance = "+str(instance))
     myresult = mycursor.fetchone()
     currentMission = json.loads(myresult[2])["mission"]["name"]
-    db.commit()
     return currentMission 
 
-playerCount = getPlayerCount(2)
-currentMission = getCurrentMission(2)
 while True:
+    playerCount = getPlayerCount(2)
+    currentMission = getCurrentMission(2)
     time.sleep(5)
     print(playerCount)
     print(currentMission)
-    db.commit()
